0a0d18c0a375421be7cc50075748226d3353c7d7
Roi Gerber <85450521+RoiGerber@users.noreply.github.com>
2025-02-06 15:49:42 +0200

    Update layout.tsx to include favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import './globals.css'
-import { Heebo } from 'next/font/google'
+import './globals.css';
+import { Heebo } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import { AuthProvider } from '@/lib/auth';
 
-const heebo = Heebo({ subsets: ['hebrew', 'latin'] })
+const heebo = Heebo({ subsets: ['hebrew', 'latin'] });
 
 export const metadata = {
   title: 'Tsalamim',
   description: 'connects photographers and their clients',
-}
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-  
   return (
     <html lang="he" dir="rtl">
       <head>
-      <link href="https://fonts.googleapis.com/css2?family=Alef&display=swap" rel="stylesheet"/>
+        <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <AuthProvider>
