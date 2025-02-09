b84ad40f05f0f72b080e95dbd755d4416aba2805
David Rechtman <davidrek@gmail.com>
2025-02-09 21:10:05 +0200

    add meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Heebo } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import { AuthProvider } from '@/lib/auth';
+import Script from 'next/script';
 
 const heebo = Heebo({ subsets: ['hebrew', 'latin'] });
 
@@ -45,6 +46,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         {/* גוגל פונטים */}
         <link href="https://fonts.googleapis.com/css2?family=Alef&display=swap" rel="stylesheet" />
+        {/* Meta Pixel Code */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1349648316212208');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1349648316212208&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
       </head>
       <body>
         <AuthProvider>
